6210b6aa663115612972b7de0092ce9a3d0262db
Fabian Klümpers <fabian.kluempers@udo.edu>
2022-07-11 21:21:54 +0200

    Add solution for Day03

---
+++ src/main/java/de/fabian/kluempers/aoc_2020/Day03.java
@@ -0,0 +1,51 @@
+package de.fabian.kluempers.aoc_2020;
+
+import io.vavr.collection.List;
+
+public class Day03 extends Puzzle<Long, Long> {
+  public Day03(List<String> originalInput) {
+    super(originalInput);
+  }
+
+  List<char[]> forest = originalInput.map(String::toCharArray);
+
+  static long traverse(List<char[]> list, TobogganConfig config, int index, long acc) {
+    return list.isEmpty() ? acc : traverse(
+        list.drop(config.yStep()),
+        config,
+        index + config.xStep(),
+        acc + mapSymbol(wrappingGet(list.head(), index))
+    );
+  }
+
+  private record TobogganConfig(int xStep, int yStep) {
+  }
+
+  @Override
+  public Long part1() {
+    return traverse(forest, new TobogganConfig(3, 1), 0, 0);
+  }
+
+  @Override
+  public Long part2() {
+    return List.of(
+        traverse(forest, new TobogganConfig(1, 1), 0, 0),
+        traverse(forest, new TobogganConfig(3, 1), 0, 0),
+        traverse(forest, new TobogganConfig(5, 1), 0, 0),
+        traverse(forest, new TobogganConfig(7, 1), 0, 0),
+        traverse(forest, new TobogganConfig(1, 2), 0, 0)
+    ).reduce(Math::multiplyExact);
+  }
+
+  private static int mapSymbol(char symbol) {
+    return switch (symbol) {
+      case '.' -> 0;
+      case '#' -> 1;
+      default -> throw new IllegalArgumentException("can't map " + symbol);
+    };
+  }
+
+  private static char wrappingGet(char[] arr, int index) {
+    return arr[index % arr.length];
+  }
+}
--- src/main/java/de/fabian/kluempers/aoc_2020/Main.java
+++ src/main/java/de/fabian/kluempers/aoc_2020/Main.java
@@ -3,10 +3,11 @@ package de.fabian.kluempers.aoc_2020;
 import io.vavr.collection.List;
 
 public class Main {
+  @SuppressWarnings("rawtypes")
   public static void main(String[] args) {
-    int day = 2;
+    int day = 3;
     List<String> input = InputReader.read(String.format("input/Day%02d.txt", day));
-    Puzzle<Integer, Integer> puzzle = new Day02(input);
+    Puzzle puzzle = new Day03(input);
     System.out.println(puzzle.part1());
     System.out.println(puzzle.part2());
   }
