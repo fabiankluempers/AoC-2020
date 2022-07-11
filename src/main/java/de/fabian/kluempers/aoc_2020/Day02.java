646970555a18b43e3b133abf6ad2f23c368e6efc
Fabian Klümpers <fabian.kluempers@udo.edu>
2022-07-11 18:46:47 +0200

    Add solution for Day02

---
+++ src/main/java/de/fabian/kluempers/aoc_2020/Day02.java
@@ -0,0 +1,54 @@
+package de.fabian.kluempers.aoc_2020;
+
+import io.vavr.collection.List;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import static java.util.function.Predicate.*;
+
+public class Day02 extends Puzzle<Integer, Integer> {
+
+  public Day02(List<String> originalInput) {
+    super(originalInput);
+  }
+  private record Password(int lower, int upper, char character, String password) {
+    static Pattern inputPattern = Pattern.compile("(\\d+)-(\\d+)\\s(\\w):\\s(\\w+)");
+
+    boolean fulfillsP1Policy() {
+      int count = List.ofAll(password.toCharArray()).count(isEqual(character));
+      return lower <= count && count <= upper;
+    }
+
+    boolean fulfillsP2Policy() {
+      return password.charAt(lower - 1) == character ^ password.charAt(upper - 1) == character;
+    }
+
+    @SuppressWarnings("unused")
+    static Password parse(String input) {
+      Matcher matcher = inputPattern.matcher(input);
+      if (matcher.find()) {
+        return new Password(
+            Integer.parseInt(matcher.group(1)),
+            Integer.parseInt(matcher.group(2)),
+            matcher.group(3).charAt(0),
+            matcher.group(4)
+        );
+      } else throw new IllegalArgumentException();
+    }
+  }
+
+  private final List<Password> passwords = originalInput.map(Password::parse);
+
+  @Override
+  public Integer part1() {
+    return passwords.count(Password::fulfillsP1Policy);
+  }
+
+  @Override
+  public Integer part2() {
+    return passwords.count(Password::fulfillsP2Policy);
+  }
+}
+
+
--- src/main/java/de/fabian/kluempers/aoc_2020/Main.java
+++ src/main/java/de/fabian/kluempers/aoc_2020/Main.java
@@ -1,12 +1,12 @@
 package de.fabian.kluempers.aoc_2020;
 
-import java.util.List;
+import io.vavr.collection.List;
 
 public class Main {
   public static void main(String[] args) {
-    int day = 1;
+    int day = 2;
     List<String> input = InputReader.read(String.format("input/Day%02d.txt", day));
-    Puzzle<Integer, Integer> puzzle = new Day01(input);
+    Puzzle<Integer, Integer> puzzle = new Day02(input);
     System.out.println(puzzle.part1());
     System.out.println(puzzle.part2());
   }
